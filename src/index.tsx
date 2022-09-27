0c34af2b20da4caf8be57359ebd8ef9319a9a726
Elías Heresi <elias.heresi@gmail.com>
2022-09-27 18:55:34 -0300

    Passed graphql context to the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { setContext } from "@apollo/client/link/context";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import reportWebVitals from "./reportWebVitals";
+
+const httpLink = createHttpLink({
+  uri: "https://hacker-graphql-eht.onrender.com/",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.REACT_APP_TOKEN;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
--- src/redux/thunks/metricThunks/metricThunks.ts
+++ src/redux/thunks/metricThunks/metricThunks.ts
@@ -3,6 +3,5 @@ import { feedQuery } from "../../../graphql/queries/queries";
 import { AppDispatch } from "../../store/store";
 
 export const loadMetricsThunk = () => async (dispatch: AppDispatch) => {
-  const metricRoute = `${process.env.REACT_APP_API_URL}`;
   const { data } = useQuery(feedQuery({ take: 1 }));
 };
