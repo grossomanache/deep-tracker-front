8574a61e5f373f0fd897e7a6a41f87b9579e9cb9
Elías Heresi <elias.heresi@gmail.com>
2022-09-27 15:42:03 -0300

    Created and tested pagination slicer

---
+++ src/redux/features/pageSlice.test.ts
@@ -0,0 +1,122 @@
+import pageReducer, {
+  changePageActionCreator,
+  changePerPageActionCreator,
+  nextPageActionCreator,
+  previousPageActionCreator,
+  totalPagesActionCreator,
+} from "./pageSlice";
+
+let initialState = {
+  page: 2,
+  perPage: 6,
+  total: 100,
+  dates: {
+    startDate: 20200101,
+    endDate: 20210101,
+  },
+};
+
+describe("Given the nextPageActionCreator", () => {
+  describe("When invoked", () => {
+    test("Then the diary state should change increase the page number", () => {
+      const expectedState = {
+        page: 3,
+        perPage: 6,
+        total: 100,
+        dates: {
+          startDate: 20200101,
+          endDate: 20210101,
+        },
+      };
+
+      const action = nextPageActionCreator;
+      const loadedState = pageReducer(initialState, action);
+
+      expect(loadedState).toEqual(expectedState);
+    });
+  });
+});
+
+describe("Given the previousPageActionCreator", () => {
+  describe("When invoked", () => {
+    test("Then the diary state should change decrease the page number", () => {
+      const expectedState = {
+        page: 1,
+        perPage: 6,
+        total: 100,
+        dates: {
+          startDate: 20200101,
+          endDate: 20210101,
+        },
+      };
+
+      const action = previousPageActionCreator;
+      const loadedState = pageReducer(initialState, action);
+
+      expect(loadedState).toEqual(expectedState);
+    });
+  });
+});
+
+describe("Given the totalEntriesActionCreator", () => {
+  describe("When invoked with a payload of 10", () => {
+    test("Then the total property in the diary state should change to this number", () => {
+      const expectedState = {
+        page: 2,
+        perPage: 6,
+        total: 10,
+        dates: {
+          startDate: 20200101,
+          endDate: 20210101,
+        },
+      };
+
+      const action = totalPagesActionCreator(10);
+      const loadedState = pageReducer(initialState, action);
+
+      expect(loadedState).toEqual(expectedState);
+    });
+  });
+});
+
+describe("Given the changePageActionCreator", () => {
+  describe("When invoked with a payload of 3", () => {
+    test("Then the diary state should change to this page number", () => {
+      const expectedState = {
+        page: 3,
+        perPage: 6,
+        total: 100,
+        dates: {
+          startDate: 20200101,
+          endDate: 20210101,
+        },
+      };
+
+      const action = changePageActionCreator(3);
+      const loadedState = pageReducer(initialState, action);
+
+      expect(loadedState).toEqual(expectedState);
+    });
+  });
+});
+
+describe("Given the changePerPageActionCreator", () => {
+  describe("When invoked with a payload of 3", () => {
+    test("Then the diary state should change to this page number", () => {
+      const expectedState = {
+        page: 2,
+        perPage: 3,
+        total: 100,
+        dates: {
+          startDate: 20200101,
+          endDate: 20210101,
+        },
+      };
+
+      const action = changePerPageActionCreator(3);
+      const loadedState = pageReducer(initialState, action);
+
+      expect(loadedState).toEqual(expectedState);
+    });
+  });
+});
---
+++ src/redux/features/pageSlice.ts
@@ -0,0 +1,57 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { PaginationState } from "../interfaces/PaginationInterfaces";
+
+const todaysDate = new Date();
+let previousDate = new Date();
+previousDate.setMonth(todaysDate.getMonth() - 24);
+
+const initialState = {
+  page: 1,
+  perPage: 4,
+  total: 0,
+} as PaginationState;
+
+const pageSlice = createSlice({
+  name: "pagination",
+  initialState,
+  reducers: {
+    totalEntries: (
+      pagination: PaginationState,
+      action: PayloadAction<number>
+    ) => ({
+      ...pagination,
+      total: action.payload,
+    }),
+    nextPage: (pagination: PaginationState) => ({
+      ...pagination,
+      page: pagination.page + 1,
+    }),
+    previousPage: (pagination: PaginationState) => ({
+      ...pagination,
+      page: pagination.page - 1,
+    }),
+    changePage: (
+      pagination: PaginationState,
+      action: PayloadAction<number>
+    ) => ({
+      ...pagination,
+      page: action.payload,
+    }),
+    changePerPage: (
+      pagination: PaginationState,
+      action: PayloadAction<number>
+    ) => ({
+      ...pagination,
+      perPage: action.payload,
+    }),
+  },
+});
+
+export const {
+  totalEntries: totalPagesActionCreator,
+  nextPage: nextPageActionCreator,
+  previousPage: previousPageActionCreator,
+  changePage: changePageActionCreator,
+  changePerPage: changePerPageActionCreator,
+} = pageSlice.actions;
+export default pageSlice.reducer;
