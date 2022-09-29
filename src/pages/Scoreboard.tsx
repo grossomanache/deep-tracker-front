81247caddcfc2a6ffb58161610d255dca6c643b7
Elías Heresi <elias.heresi@gmail.com>
2022-09-29 15:35:52 -0300

    Added reducer for all of the different data layers

--- src/pages/Scoreboard.tsx
+++ src/pages/Scoreboard.tsx
@@ -1,11 +1,23 @@
 import { useQuery } from "@apollo/client";
 import { feedQuery } from "../graphql/queries/queries";
+import { Metric } from "../redux/interfaces/MetricsInterfaces";
 
 const Scoreboard = (): JSX.Element => {
   const usedQuery = feedQuery({ take: 1 });
   const { data } = useQuery(usedQuery);
+  console.log(data);
 
-  return <></>;
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed.metrics.map((metric: Metric) => (
+            <p>{metric.date.toISOString()}</p>
+          ))}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Scoreboard;
