da94b1b78ca6f9e7f7e5ae44a03c5e7180d63d0b
Elías Heresi <elias.heresi@gmail.com>
2022-09-30 16:02:51 -0300

    Renamed variables and used find instead of filter

--- src/utils/convertToTable.ts
+++ src/utils/convertToTable.ts
@@ -2,27 +2,22 @@ import { Metric } from "../redux/interfaces/MetricsInterfaces";
 import { uniqueDates, uniqueValues } from "./extractUnique";
 
 export const convertToTable = (metrics: Metric[]) => {
-  const headers = ["Date"];
   const leadMeasures = uniqueValues(metrics, "name");
-
-  leadMeasures.forEach((leadMeasure) => {
-    headers.push(leadMeasure);
-  });
-
   const allDates = uniqueDates(metrics);
 
+  const headers = ["Date", ...leadMeasures];
   const convertedTable: (number | string)[][] = [headers];
 
   allDates.forEach((date) => {
-    const loggedValues: (number | string)[] = [date];
+    const tableRow: (number | string)[] = [date];
     leadMeasures.forEach((leadMeasure) => {
-      const searchedMetric = metrics.filter(
+      const searchedMetric = metrics.find(
         ({ name, date: loggedDate }) =>
           name === leadMeasure && loggedDate.toString().slice(0, 10) === date
       );
-      loggedValues.push(searchedMetric[0] ? searchedMetric[0].value : "N/A");
+      tableRow.push(searchedMetric ? searchedMetric.value : "N/A");
     });
-    convertedTable.push(loggedValues);
+    convertedTable.push(tableRow);
   });
 
   return convertedTable;
