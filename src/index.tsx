a17520818f5f93fe000a3ecc9502c48ed410e4c8
Elías Heresi <elias.heresi@gmail.com>
2022-10-09 10:34:10 -0300

    Modularized the graphql client to a unique file

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { setContext } from "@apollo/client/link/context";
-import {
-  ApolloProvider,
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-} from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
-
-const httpLink = createHttpLink({
-  uri: process.env.REACT_APP_API_URL,
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = process.env.REACT_APP_TOKEN;
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import { client } from "./graphql/client/client";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
